b4a07501998d23fc2e59aaffd42a893e1ba042d1
mmetcalf9 <masonmetcalf9@gmail.com>
2020-06-18 14:08:45 -0400

    This is an example layout.

---
+++ app/src/main/java/com/example/strictlygains/Fragment1.java
@@ -0,0 +1,29 @@
+package com.example.strictlygains;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.widget.Button;
+
+public class Fragment1 extends Fragment implements View.OnClickListener{
+    Button myButton;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment1_layout, container, false);
+        myButton = view.findViewById(R.id.button);
+        myButton.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        myButton.setText("Working");
+    }
+}
