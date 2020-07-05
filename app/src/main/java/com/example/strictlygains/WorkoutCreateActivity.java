beb37f32fbba5ecb1f3e660ee788ff557193b098
jcwasher <62572358+jcwasher@users.noreply.github.com>
2020-07-05 19:39:25 -0400

    Exercise List
    Changed the way default exercises are added (via txt file).

--- app/src/main/java/com/example/strictlygains/WorkoutCreateActivity.java
+++ app/src/main/java/com/example/strictlygains/WorkoutCreateActivity.java
@@ -10,13 +10,21 @@ import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.example.strictlygains.data.Exercise;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WorkoutCreateActivity extends AppCompatActivity {
     SearchView search;
     ListView wList;
     ArrayList<String> list;
+    ArrayList<Exercise> exerciseList;
     ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,15 +33,23 @@ public class WorkoutCreateActivity extends AppCompatActivity {
         search = findViewById(R.id.searchView);
         wList = findViewById(R.id.workoutList);
         list = new ArrayList<String>();
+        exerciseList = new ArrayList<Exercise>();
 
-        list.add("Dead Lift");
-        list.add("Squat");
-        list.add("Bench Press");
-        list.add("Pull-up");
-        list.add("Overhead Press");
-        list.add("Push-up");
-        list.add("Leg Press");
-        list.add("Lunges");
+        try{
+            Scanner read = new Scanner( getAssets().open("exercises.txt") );
+            read.useDelimiter(",|\\n");
+
+            while( read.hasNext() )
+                exerciseList.add(new Exercise( read.nextInt(), read.next(), read.next() ));
+
+            for(int i = 0; i < exerciseList.size(); i++)
+                list.add( exerciseList.get(i).getName() );
+
+            read.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
         wList.setAdapter(adapter);
