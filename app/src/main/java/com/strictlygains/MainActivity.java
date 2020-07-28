7bfec60ff7cbcffe2c21dd100679a2c4ff42de22
mtn18 <nguyenmichael415@gmail.com>
2020-07-28 00:33:38 -0400

    Added drawer and fixed a few things.

--- app/src/main/java/com/strictlygains/MainActivity.java
+++ app/src/main/java/com/strictlygains/MainActivity.java
@@ -1,20 +1,31 @@
 package com.strictlygains;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.strictlygains.ui.login.LoginActivity;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
-import androidx.appcompat.app.AppCompatActivity;
 
+import com.strictlygains.ui.login.LoginActivity;
 import com.strictlygains.ui.main.SectionsPagerAdapter;
 
 import java.io.File;
@@ -22,22 +33,44 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    SectionsPagerAdapter sectionsPagerAdapter;
+    ViewPager viewPager;
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+    TabLayout tabs;
+
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
+        sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        TabLayout tabs = findViewById(R.id.tabs);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
- //       FloatingActionButton fab = findViewById(R.id.fab);
+
+
+        // invert
+        ViewCompat.setLayoutDirection(tabs, ViewCompat.LAYOUT_DIRECTION_LTR);
+        ViewCompat.setLayoutDirection(viewPager, ViewCompat.LAYOUT_DIRECTION_LTR);
 
         // copy the defaultExercises to local files if it does not yet exist
         if(!new File(getFilesDir(), "defaultExercises").exists())
@@ -49,26 +82,25 @@ public class MainActivity extends AppCompatActivity {
             assert exerciseList != null;
             DataHelper.updateExerciseHistory(this, exerciseList);
         }
-
-
-
-
-
-
-    /*   fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));      // TEMPORARY: used to access login screen.
-
-
-            }
-
-        }); */
     }
 
     @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        switch(id) {
+            case R.id.nav_login:
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                break;
+        }
+
+        return false;
+    }
+/*
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.login_menu, menu);
+        getMenuInflater().inflate(R.menu.nav_menu, menu);
         return true;
     }
 
@@ -84,6 +116,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
+*/
 
 }
